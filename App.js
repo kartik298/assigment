27126cf886304b087bd844f211ec1904bc83820c
Kartik Agarwal <56475405+kartik298@users.noreply.github.com>
2023-06-28 13:53:20 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Todo from "./Todo";
+import Pomodoro from './Pomodoro';
+function App() {
+  return (
+    <div className="App">
+       Lets start
+      <Todo></Todo>
+    </div>
+  );
+}
+
+export default App;
